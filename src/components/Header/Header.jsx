c4f7af7dde7fffe1ff7047c26d396878d4dc6920
Oleh Didyk <braun.ufo@gmail.com>
2022-06-09 01:31:02 +0300

    added menu items list

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,15 @@ const Header = () => {
   return (
     <header className={s.header}>
       <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Apple_logo_black.svg/1667px-Apple_logo_black.svg.png" alt="apple"/>
+      <div>
+        <ul>
+          <li className="mark-1">Aas</li>
+          <li className="mark-2">asdassad</li>
+          <li className="mark-3">asdsadasd</li>
+          <li className="mark-4">asdsa</li>
+          <li className="mark-5">asdasd</li>
+        </ul>
+      </div>
     </header>
   )
 }
